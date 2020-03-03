bc1cc2d786720cd3709df3edc8685bc304778355
Sonny Immanuel Hutabarat <hutabarat2011@gmail.com>
2020-03-03 11:35:28 +0700

    first commit

---
+++ app/src/main/java/com/example/terminalagro/Splashscreen.kt
@@ -0,0 +1,12 @@
+package com.example.terminalagro
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Splashscreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splashscreen_activity)
+    }
+}
